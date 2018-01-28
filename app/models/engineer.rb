76781d991f78727218f3ec4fa6b649785ba715d4
imaprog <shafiqaziz06@gmail.com>
2018-01-28 20:09:01 +0800

    merge duplicates validate email lines

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,9 +1,8 @@
 class Engineer < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	
+
 	has_many :on_call_works
 
 	validates :name, presence: true
-	validates :email, presence: true
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
